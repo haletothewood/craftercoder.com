6b1e876f7c93deb322a32c5b3b6ee059514b720a
David Halewood <davidnhalewood@hotmail.com>
2019-07-04 13:29:26 +0100

    Remove submodule and add gatsby to main git repo

---
+++ craftercoder.com/craftercoder.com/src/components/header.js
@@ -0,0 +1,43 @@
+import { Link } from "gatsby"
+import PropTypes from "prop-types"
+import React from "react"
+
+const Header = ({ siteTitle }) => (
+  <header
+    style={{
+      background: `#282828`,
+      borderBottom: `solid 2px #1d1d1d`,
+      marginBottom: `1.45rem`,
+    }}
+  >
+    <div
+      style={{
+        margin: `0 auto`,
+        maxWidth: 960,
+        padding: `1.45rem 1.0875rem`,
+      }}
+    >
+      <h1 style={{ margin: 0 }}>
+        <Link
+          to="/"
+          style={{
+            color: `#d79921`,
+            textDecoration: `none`,
+          }}
+        >
+          {siteTitle}
+        </Link>
+      </h1>
+    </div>
+  </header>
+)
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Header.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Header
