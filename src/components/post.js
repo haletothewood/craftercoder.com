07c7beaaa7d340a0cc199cedbb28beb79717bc09
David Halewood <davidnhalewood@hotmail.com>
2019-09-04 21:57:01 +0100

    Add read time to blog post listings

--- src/components/post.js
+++ src/components/post.js
@@ -10,6 +10,9 @@ export default function Post({ post }) {
         <Link to={post.frontmatter.path}>{post.frontmatter.title}</Link>
       </h1>
       <h2 className="blog-post-date">{post.frontmatter.date}</h2>
+      <div className="blog-post-reading-time">
+        {post.fields.readingTime.text}
+      </div>
       <p>{post.excerpt}</p>
     </div>
   )
