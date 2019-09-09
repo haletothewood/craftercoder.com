8c26047f6dc696bf8a409b1186832d995fe1c4cb
David Halewood <davidnhalewood@hotmail.com>
2019-09-09 16:30:19 +0100

    :wrench: Refactor max width for images in blog posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,7 +62,7 @@ module.exports = {
           {
             resolve: `gatsby-remark-images`,
             options: {
-              maxWidth: 1200,
+              maxWidth: 600,
             },
           },
           `gatsby-remark-reading-time`,
